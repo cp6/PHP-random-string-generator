9f674a1e7270771530488fbf925490aca95ecefa
cp6 <cp6@users.noreply.github.com>
2020-01-11 11:36:31 +1100

    Add class.php
    Add the stringGen class

---
+++ class.php
@@ -0,0 +1,52 @@
+<?php
+
+class stringGen
+{
+    public int $character_count;
+    public string $characters_type;
+    public string $custom_characters;
+
+    /**
+     * Set the parameters for the string to be generated
+     * @param int $character_count amount of characters to be in the string
+     * @param string $characters_type ALL | ALLNOSYMBOLS | NUMBERS | LOWERCASE | UPPERCASE | SYMBOLS
+     * @param string $custom_characters Use only characters from here, leave empty to ignore
+     */
+    public function __construct($character_count = 16, $characters_type = 'ALL', $custom_characters = '')
+    {
+        $this->character_count = $character_count;
+        $this->characters_type = $characters_type;
+        $this->custom_characters = $custom_characters;
+    }
+
+    /**
+     * Returns the string as per defined
+     * @return string
+     */
+    public function generate()
+    {
+        if ($this->custom_characters == '') {
+            if ($this->characters_type == 'ALL') {
+                $character_pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+=~[]{}\|<>?/';
+            } elseif ($this->characters_type == 'ALLNOSYMBOLS') {
+                $character_pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            } elseif ($this->characters_type == 'NUMBERS') {
+                $character_pool = "0123456789";
+            } elseif ($this->characters_type == 'LOWERCASE') {
+                $character_pool = "abcdefghijklmnopqrstuvwxyz";
+            } elseif ($this->characters_type == 'UPPERCASE') {
+                $character_pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            } elseif ($this->characters_type == 'SYMBOLS') {
+                $character_pool = "!@#$%^&*()-_+=~[]{}\|<>?/";
+            }
+        } else {
+            $character_pool = $this->custom_characters;
+        }
+        $char_length = strlen($character_pool);
+        $the_string = '';
+        for ($i = 0; $i < $this->character_count; $i++) {
+            $the_string .= $character_pool[mt_rand(0, $char_length - 1)];
+        }
+        return $the_string;
+    }
+}
